a719c9ed7f714a3650ef9d84f8045522bcc21818
Parano9I <medoc1948@gmail.com>
2022-06-28 22:13:41 +0300

    Task 4: Use conditional constucts

--- index.php
+++ index.php
@@ -13,64 +13,24 @@ $langs = [
 ];
 
 $users = [
-  "40" => ["name" => "Vlad", "email" => "vlad@gmail.com"],
-  "2" => ["name" => "Anton", "email" => "anton3434@gmail.com"],
-  "0" => ["name" => "Eduard", "email" => "edic8345@gmail.com"],
-  "29" => ["name" => "Roman", "email" => "romaha234@gmail.com"],
-  "18" => ["name" => "Rostik", "email" => "bugiman34@gmail.com"],
-  "6" => ["name" => "Mike", "email" => "milki34@gmail.com"],
+  "40" => ["name" => "Vlad", "email" => "vlad@gmail.com", "lang" => "fr"],
+  "2" => ["name" => "Anton", "email" => "anton3434@gmail.com", "lang" => "ru"],
+  "0" => ["name" => "Eduard", "email" => "edic8345@gmail.com", "lang" => "en"],
+  "29" => ["name" => "Roman", "email" => "romaha234@gmail.com", "lang" => "fr"],
+  "18" => ["name" => "Rostik", "email" => "bugiman34@gmail.com", "lang" => "de"],
+  "6" => ["name" => "Mike", "email" => "milki34@gmail.com", "lang" => "de"],
 ];
 
-echo "Number of users ". count($users);
+$greetings = [
+  "ua" => "Привіт",
+  "en" => "Hello",
+  "fr" => "Bonjour",
+  "de" => "Hallo",
+  "ru" => "Привет",
+];
 
-krsort($users);
-
-$userWithMinId = $users[min(array_keys($users))];
-$userWithMaxId = $users[max(array_keys($users))];
-
-function getArrayElement(array $array, int $shiftElement): array{
-  
-  if(!$shiftElement) return 0;
-
-  $shiftMode = gmp_sign($shiftElement);
-  if($shiftMode === -1) end($array);
-   
-  for($i = 0; $i < abs($shiftElement); $i++){
-    
-    $shiftMode === 1 ? next($array) : prev($array);
-
-  };
-
-  return current($array);
-};
-
- $userWithPreMaxId = getArrayElement($users, 1);
- $userWithAfterMinId = getArrayElement($users, -1);
-
-echo "<br>";
-echo "<pre>";
-  echo "Sort array by DESC <br>";
-  print_r($users);
-echo "</pre>";
-
-echo "<br>";
-echo "<pre>";
-  echo "userWithMinId";
-  print_r($userWithMinId);
-  echo "userWithMaxId";
-  print_r($userWithMaxId);
-  echo "userWithPreMaxId";
-  print_r($userWithPreMaxId);
-  echo "userWithAfterMinId";
-  print_r($userWithAfterMinId);
-echo "</pre>";
-
-unset($users[min(array_keys($users))]);
-
-echo "<pre>";
-  echo "new array after unset method";
-  print_r($users);
-echo "</pre>";
+$langFirstUser = $users[40]["lang"];
+$langLastUser = $users[6]["lang"];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -88,6 +48,19 @@ echo "</pre>";
     <h2 class="card-heading">
       Sign up
     </h2>
+    <?php if($langFirstUser === $langLastUser) : ?>
+    <h2 class="sub-title">
+      <?php echo $greetings[$langFirstUser] ?>
+    </h2>
+    <?php endif; ?>
+    <?php if($langFirstUser !== $langLastUser) : ?>
+    <h2 class="sub-title">
+      <?php echo $greetings[$langFirstUser] ?>
+    </h2>
+    <h2 class="sub-title">
+      <?php echo $greetings[$langLastUser] ?>
+    </h2>
+    <?php endif; ?>
     <form class="card-form">
       <div class="input">
         <input type="text" class="input-field" value="<?php echo $username ?>" required />
