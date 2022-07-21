a4357bf198e4a62a1cf651efe369e188fa93e617
Parano9I <medoc1948@gmail.com>
2022-07-21 12:01:45 +0300

    Created commons sql queries

--- db/queries.sql
+++ db/queries.sql
@@ -50,3 +50,69 @@ CREATE USER 'client'@'localhost' IDENTIFIED BY 'client';
 GRANT SELECT, INSERT ON shop_lamp_dev.* TO 'client'@'localhost';
 
 ///////////////////////////////////////////////////////////////////////
+
+INSERT INTO `categories`(`name`) 
+VALUES
+    ('smartphones'),
+    ('laptops'),
+    ('fragrances'),
+    ('skincare')
+;
+
+ALTER TABLE `users` MODIFY `email` VARCHAR(150); // У рядку 20 вказав не той тип даних
+
+INSERT INTO `users`(`login`, `email`, `password`) 
+VALUES
+    ('anton16', 'anton16@gmail.com', 'anton16'),
+    ('vladErg', 'vladErg@gmail.com', 'vladErg'),
+    ('oleg7f', 'oleg7f@gmail.com', 'oleg7f'),
+    ('$asha', '$asha@gmail.com', '$asha')
+;
+
+INSERT INTO `products`(`title`, `price`, `amount`, `category_id`) 
+VALUES
+    ('iPhone 9', '549', '94', '1'),
+    ('iPhone X', '899', '34', '1'),
+    ('Samsung Universe 9', '1249', '36', '1'),
+    ('MacBook Pro', '1749', '83', '2'),
+    ('Samsung Galaxy Book', '1499', '50', '2'),
+    ('perfume Oil', '13', '65', '3'),
+    ('Brown Perfume', '40', '52', '3'),
+    ('Fog Scent Xpressio Perfume', '13', '61', '3'),
+    ('Hyaluronic Acid Serum', '19', '110', '4'),
+    ('Tree Oil 30ml', '12', '78', '4')
+;
+
+INSERT INTO `carts`(`product_id`, `user_id`, `amount`) 
+VALUES
+    ('4', '2', '1'),
+    ('1', '2', '3'),
+    ('8', '2', '10'),
+    ('2', '3', '3'),
+    ('1', '3', '1'),
+    ('4', '1', '5'),
+    ('2', '1', '2')
+;
+
+SELECT COUNT(*) FROM products; // загальна кількість товарів
+
+SELECT * FROM products
+    WHERE amount = (SELECT MAX(amount) FROM products)
+; // товар з максимальною кількістю на складі
+
+UPDATE products 
+    SET products.amount = products.amount * 2 
+    WHERE products.amount = (SELECT amount FROM (SELECT MIN(amount) FROM products) AS product)
+; // збільшення значення кількості товару з найменшою кількістю
+
+SELECT user.login, product.title, product.price, cart.amount FROM carts cart
+    INNER JOIN users user ON cart.user_id = user.id
+    INNER JOIN products product ON cart.product_id = product.id
+;  // користувачі та їх товари у кошику
+
+SELECT tmp_table.count_prod, c.name FROM (
+	SELECT COUNT(p.id) AS count_prod, p.category_id FROM products p
+	GROUP BY p.category_id  
+) tmp_table 
+INNER JOIN categories c ON tmp_table.category_id = c.id
+ORDER BY tmp_table.count_prod DESC; // відсортована в порядку зменшення, кількість унікального товару у категорії 
